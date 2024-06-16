52906f08ef9dcdd015064b4247f7e6da07f6e8f7
Brandon <brandon.ohboil@gmail.com>
2024-06-16 01:36:10 -0400

    flowbite installation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+  } from "flowbite-svelte";
+</script>
+
+<Navbar>
+  <NavBrand href="/">
+    <img
+      src="/src/assets/loft-logo.png"
+      class="me-3 h-6 sm:h-9"
+      alt="Loft Logo"
+    />
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+    ></span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/menu">Pricing</NavLi>
+    <NavLi href="/games">Games</NavLi>
+    <NavLi href="/events">Events</NavLi>
+    <NavLi href="/trivia">Trivia</NavLi>
+  </NavUl>
+</Navbar>
