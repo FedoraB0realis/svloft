3e4b92937d473b6cf2a22ef3a11504ba8eac7078
Brandon <brandon.ohboil@gmail.com>
2024-06-19 17:27:35 -0400

    moved file structure

---
+++ src/components/Main.svelte
@@ -0,0 +1,15 @@
+<script>
+  import NavBar from "../components/NavBar.svelte";
+  import { Button, Card } from 'flowbite-svelte';
+  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+</script>
+
+<NavBar />
+<h1>Welcome</h1>
+<Card href="/cards">
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">For first time visitors</h5>
+  <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight mb-4">In order to access our library of Board and Video games, an individual 9$ Stay & Play fee is applied</p>
+  <Button class="w-fit">
+    For more important info <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
+  </Button>
+</Card>
